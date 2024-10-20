8f5fc1fe230f1d8a6edb7f5e0b3f2614e7da0cc6
Erisula <junderwood7415@gmail.com>
2024-10-19 20:54:59 -0600

    Add files via upload

---
+++ pet-store/target/classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO customer (first_name) VALUES ('Jennifer');
+INSERT INTO customer (first_name) VALUES ('Brian');
+INSERT INTO customer (first_name) VALUES ('Jacob');
+INSERT INTO customer (first_name) VALUES ('Josh');
+INSERT INTO customer (first_name) VALUES ('Lauren');
+INSERT INTO customer (first_name) VALUES ('Brandy');
+INSERT INTO customer (first_name) VALUES ('Chris');
